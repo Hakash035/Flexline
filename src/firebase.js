bb7da5a939d1e71c19884f6a77c45af2b60f38f7
Hakash035 <42975048+Hakash035@users.noreply.github.com>
2024-06-16 09:38:12 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
  import firebase from 'firebase'
  
  var firebaseConfig = {
-    apiKey: "AIzaSyCGI560fCyBsjCy3FFdw1IurKemUO_aRyU",
-    authDomain: "flexline-2a401.firebaseapp.com",
-    projectId: "flexline-2a401",
-    storageBucket: "flexline-2a401.appspot.com",
-    messagingSenderId: "815519610240",
-    appId: "1:815519610240:web:8671f79f5b08f858f7fd65",
-    measurementId: "G-CD6STEG252"
+    // Firebase Configs
   };
 
   const app = firebase.initializeApp(firebaseConfig);
